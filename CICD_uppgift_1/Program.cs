e829fc235f2148d8c3e081d3ceb16dabb6534251
Oskar Kling <oskarkling@gmail.com>
2021-11-11 13:46:17 +0100

    Add project files.

---
+++ CICD_uppgift_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CICD_uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+    }
+}
---
+++ CICD_uppgift_1Tests/TestMethodTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CICD_uppgift_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICD_uppgift_1.Tests
+{
+    [TestClass()]
+    public class TestMethodTests
+    {
+        [TestMethod()]
+        public void AddIntTogetherTest()
+        {
+            TestMethod test = new TestMethod();
+            int ans = test.AddIntTogether(2, 2);
+            Assert.AreEqual(4, ans);
+        }
+    }
+}

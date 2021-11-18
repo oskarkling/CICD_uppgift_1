ea48532dc80787eb74193d819752a233298359d0
Oskar Kling <oskarkling@gmail.com>
2021-11-18 11:54:29 +0100

    small test again for workflow

--- CICD_uppgift_1/Core.cs
+++ CICD_uppgift_1/Core.cs
@@ -6,7 +6,7 @@ namespace CICD_uppgift_1
     {
         public void Run()
         {
-            System.Console.WriteLine("small test change");
+            System.Console.WriteLine("small test change again");
             MainMenu();
         }
         private void MainMenu()
